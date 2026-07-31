593a06a134532bc5bb50c8f20d28843c928548dd
mazinosama02-eng <mazinosama02@gmail.com>
2026-07-31 19:00:25 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,3 +1,21 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+mongoose
+  .connect(process.env.DATABASE_STRING)
+  .then(() => {
+    console.log("the connect to DB is Successful🥳");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
+
+app.listen(3000, () => {
+  console.log("the server is running on port 300");
+});
